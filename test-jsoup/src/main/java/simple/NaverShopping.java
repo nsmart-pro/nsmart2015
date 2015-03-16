57059e7f6fd7d55e0c2d497072fc285f49d28799
nsmart-pro <nsmart.pro@gmail.com>
2015-03-16 18:54:23 +0900

    .

--- test-jsoup/src/main/java/simple/NaverShopping.java
+++ test-jsoup/src/main/java/simple/NaverShopping.java
@@ -1,6 +1,8 @@
 package simple;
 
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.util.List;
 import java.util.Map;
@@ -14,26 +16,11 @@ import org.jsoup.select.Elements;
 
 public class NaverShopping {
 	
-	public String cutString(String target){
-		String result = null;
-		
-		if(target != null || target.length() == 0){
-			for(int i = 0; i < target.length(); i++){
-				if(Character.getType(target.charAt(i)) == 5){
-					result = target.substring(0, i);
-					break;
-				}
-			}
-		}
-		
-		return result;
-	}
-	
-	public static void main(String[] args) throws Exception {
+	public void printPricesOf(String keyword) throws UnsupportedEncodingException, IOException  {
 		
 		boolean first  = true;
-		
-		String key = URLEncoder.encode("오렌지", "UTF-8"); 
+
+		String key = URLEncoder.encode(keyword, "UTF-8");
 		Document doc = Jsoup.connect("http://shopping.naver.com/search/all_search.nhn?query="+key+"&cat_id=&frm=NVSHSRC").get();
 		Elements title = doc.select("li._product_list");
 
@@ -47,5 +34,35 @@ public class NaverShopping {
 		}
 
 	}
+	
+	public void test(String keyword) throws UnsupportedEncodingException, IOException  {
+		
+		boolean first  = true;
+
+		String key = URLEncoder.encode(keyword, "UTF-8");
+		Document doc = Jsoup.connect("http://search.naver.com/search.naver?where=nexearch&query="+key+"&sm=top_hty&fbm=1&ie=utf8").get();
+		Elements title = doc.select("li._product_list");
+
+		for(Element li : title){
+			if(first) {
+				System.out.println("Product Category : " + li.select("div.info > span.depth").text());
+				first = false;
+			}
+			System.out.println(li.select("div.info > span.price > em").text() + "::: " 
+					  			+ li.select("a.tit").text() );
+		}
+
+	}
+
+	public static void main(String[] args) throws Exception {
+		
+		NaverShopping shopping = new NaverShopping();
+		
+		shopping.printPricesOf("바나나");
+		//shopping.test("운동화");
+
+	}
+	
+	
 
 }
